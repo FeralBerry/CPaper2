9a1cd5411f5fed4e04556c04721e4fd28fabe01f
FeralHorse <pusiket90@yandex.ru>
2024-01-12 13:34:39 +0300

    Базовый уровень

--- src/main/java/org/example/Services/ExaminerQuestionImpl.java
+++ src/main/java/org/example/Services/ExaminerQuestionImpl.java
@@ -3,18 +3,25 @@ package org.example.Services;
 import org.example.Question;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Random;
 @Service
 public class ExaminerQuestionImpl implements ExaminerService{
-    private Random random;
-    private QuestionService questionService;
+    private final QuestionService questionService;
     public ExaminerQuestionImpl(QuestionService questionService){
         this.questionService = questionService;
     }
 
     @Override
     public Collection<Question> getQuestions(int amount) {
-        return null;
+        if(amount > 0){
+            Collection<Question> collection = new ArrayList<>();
+            for(int i = 0; i < amount; i++){
+                collection.add(questionService.getRandomQuestion());
+            }
+            return collection;
+        } else {
+            return null;
+        }
     }
 }
--- src/main/java/org/example/Services/JavaQuestionService.java
+++ src/main/java/org/example/Services/JavaQuestionService.java
@@ -7,29 +7,54 @@ import java.util.*;
 
 @Service
 public class JavaQuestionService implements QuestionService{
-    private Set<Question> questions;
+    private final Set<Question> questions;
+    public JavaQuestionService(Set<Question> questions){
+        this.questions = questions;
+    }
     @Override
     public Question add(String question, String answer) {
-        return null;
+        Question q = new Question(question,answer);
+        boolean firstQuestion = true;
+        for (Question s :  this.questions) {
+            if(s.getQuestion().equalsIgnoreCase(question)){
+                this.questions.remove(s);
+                this.questions.add(q);
+                firstQuestion = false;
+            }
+        }
+        if(firstQuestion){
+            this.questions.add(q);
+        }
+        return q;
     }
-
     @Override
     public Question add(Question question) {
-        return null;
+        this.questions.add(question);
+        return question;
     }
-
     @Override
     public Question remove(Question question) {
+        if(this.questions.contains(question)){
+            this.questions.remove(question);
+            return question;
+        }
         return null;
     }
-
     @Override
     public Collection<Question> getAll() {
-        return null;
+        return this.questions;
     }
-
     @Override
     public Question getRandomQuestion() {
+        int size = this.questions.size();
+        int item = new Random().nextInt(size);
+        int i = 0;
+        for(Question obj : this.questions)
+        {
+            if (i == item)
+                return obj;
+            i++;
+        }
         return null;
     }
 }
